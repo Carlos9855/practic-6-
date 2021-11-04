74fc4cd5569eaccff54c4f423d6db021a3ca364b
CC <jose.camacho.s@ucb.edu.bo>
2021-11-03 22:02:12 -0400

    subir

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
 import json
 
+def Arc_consistency(nodos, constraints, domain, assignments,arcs):
+    #fill queue with initial arcs
+    fillQueue(nodos,constraints,arcs)
+    while arcs:
+        arcVars = arcs.pop(0)
+        if Domain_Touched(arcVars[0], arcVars[1], nodos, constraints, domain, assignments, arcs):
+            domainIIndex = nodos.index(arcVars[0])
+            if len(domain[domainIIndex]) == 0:
+                print ("No Solution")
+                return False
+            neighbors = list(constraints[domainIIndex])
+            neighbors.remove(arcVars[1])
+            for xk in neighbors:
+                arcs.append([xk, arcVars[0]])
+    return assignments
+
+
+#fill queue with initial arcs
+def fillQueue(nodos,constraints,arcs):
+    for var in nodos:
+        varIndex = nodos.index(var)
+        for arc in constraints[varIndex]:
+            arcs.append([var, arc])
+
+def Domain_Touched( Xi, Xj, nodos, constraints, domain, assignments,arcs):
+    touched = False
+    Iindex = nodos.index(Xi);
+    #for each value in the domain
+    for x in domain[Iindex]:
+        #check if state is adjacent, if not, go to next x
+        if Xj not in constraints[Iindex]:
+            break
+        if Xj not in assignments:
+            continue
+        #if adjacent state has current assignment of X (same color)
+        if x in assignments[Xj]:
+            domain[Iindex].remove(x)  #remove x from Di
+            touched = True
+    if not touched and len(domain[Iindex]) > 0:
+        assignments[Xi] = domain[Iindex][0]
+    return touched
+
 def main():
     isInter = False
     nodos = []
@@ -9,8 +51,10 @@ def main():
     aux1= []
     dias=["L","M","Mi","J","V"]
     salas=["A","B","C"]
+    assignments = { }
+    arcs = []
     count = 0
-    for i in range(0, 1):
+    for i in range(0, 1): #Se crea el grafo para los dias de la semana
         for x in range(1,7):
             for z in range(0,3):
                 nodos.append(dias[i]+"_"+str(x)+"_"+salas[z])  
@@ -79,10 +123,11 @@ def main():
     for i in range (0, len(domain)):
         if domain[i] != []:
             domain[i].append("null")
-    print (nodos) 
-    print (constraints) 
-    print(domain)
+            domain[i].append("null1")
+    #print (nodos) 
+    #print (constraints) 
+    #print(domain)
 
-
-
+    res = Arc_consistency(nodos, constraints, domain, assignments,arcs)
+    print (res)
 main()
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Backtrack-Backjumping
+## Context
+
+## Propose the problem as a CSP
+
+## Implementation
+
+ ## Experiments
+
+ ### Forward checking 
+
+ ### Minimum remaining values algorithm
+
+ ### Least Constrained value
+
+ ### Running the program
